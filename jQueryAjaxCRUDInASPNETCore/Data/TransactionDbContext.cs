cf34b01063573a06b98271f07b16ccf0a6144ead
DQA <quanganh21112001@gmail.com>
2022-11-29 02:29:47 +0700

    InitCommit
    Project to learn Ajax along with ASP .NET

---
+++ jQueryAjaxCRUDInASPNETCore/Data/TransactionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using jQueryAjaxCRUDInASPNETCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace jQueryAjaxCRUDInASPNETCore.Data
+{
+  public class TransactionDbContext : DbContext
+  {
+    public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+    {
+    }
+    public DbSet<TransactionModel> Transactions { get; set; }
+  }
+}
---
+++ jQueryAjaxCRUDInASPNETCore/Models/TransactionModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jQueryAjaxCRUDInASPNETCore.Models
+{
+  public class TransactionModel
+  {
+    [Key]
+    public int TransactionId { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+    public int AccountNumber { get; set; }
+
+    [Column(TypeName = "nvarchar(100)")]
+
+    public string BeneficiaryName { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+
+    public string BankName { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+    public string SWIFTCode { get; set; }
+    public int Amount { get; set; }
+    public DateTime Date { get; set; }
+  }
+}
